e1912b4463f7699b260207cf689683918e431190
CMVdeO <93734542+CMVdeO@users.noreply.github.com>
2024-12-13 11:48:34 -0300

    [ADD] Cambio en boleta para dirección seleccionada

--- pos_inherit/models/pos_config.py
+++ pos_inherit/models/pos_config.py
@@ -8,3 +8,15 @@ class PosConfig(models.Model):
         'account.journal', string='Boleta Journal',
         domain=[('type', '=', 'sale')],
         help="Accounting journal used to create Boleta.")
+
+    address_change_id = fields.Many2one('res.partner',string='Direccion boleta de POS')
+    address_change_street = fields.Char(string='Calle',related='address_change_id.street')
+    address_change_street2 = fields.Char(string='Calle 2',related='address_change_id.street2')
+    address_change_city = fields.Char(string='Ciudad',related='address_change_id.city')
+    address_change_phone = fields.Char(string='Telefono',related='address_change_id.phone')
+    address_change_activity_description = fields.Char(string='Telefono',related='address_change_id.l10n_cl_activity_description')
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    address_change_id = fields.Many2one('res.partner',related='pos_config_id.address_change_id', readonly=False)
